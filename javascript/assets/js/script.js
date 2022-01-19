a32fb372f8a7743d4fcd70f752fc1ab4f8643984
sejinskim <95746445+seockchan@users.noreply.github.com>
2022-01-19 20:47:04 +0900

    222

---
+++ javascript/assets/js/script.js
@@ -0,0 +1,6 @@
+//결과 보기 버튼을 클릭하면 경고창을 띄어주세요~
+document.querySelectorAll(".result").forEach(function(el){
+    el.addEventListener("click", function(){ 
+        el.classList.toggle("active");
+    });
+})    
